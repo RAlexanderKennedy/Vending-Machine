255d066fdf58c81861dfdd1b258d067749e9c303
Blake Washington <blakewash1@gmail.com>
2020-11-19 18:12:45 -0500

    attempting merge

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,10 +1,11 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 import com.techelevator.view.Menu;
 
-import materials.Funds;
+import materials.*;
 
 public class VendingMachineCLI {
 
@@ -12,10 +13,11 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 	private static final String[] PURCHASE_MENU = {"Feed Money", "Select Product", "Finish Transaction", "Back"};
-	private static final String[] FEED_MONEY_MENU = {"1", "5", "10", "Back"};
+	private static final String[] FEED_MONEY_MENU = {"1", "2", "5", "10", "Back"};
 
 	private Menu menu;
-	protected Funds funds = new Funds();
+	private Funds funds = new Funds();
+	private InventoryManagement inventory = new InventoryManagement();
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
@@ -36,10 +38,24 @@ public class VendingMachineCLI {
 					selection = (String) menu.getChoiceFromOptions(PURCHASE_MENU, funds.getBalance());
 				
 					if (selection.equals("Feed Money")) {
+						System.out.println("You selected " + selection);
 						processMoney();
 					}
+					
+					if (selection.equals("Select Product")) {
+						System.out.println("You selected " + selection);
+						selectProduct();
+					}
+					
+					if (selection.equals("Finish Transaction")) {
+						System.out.println("You selected " + selection);
+						funds.getChange();
+						inventory.
+						// changing value of 'selection' so that user returns to main menu
+						selection = "Back";
+					}
 				
-					System.out.println("You selected" + selection);
+					
 				}
 			}
 		}
@@ -55,22 +71,55 @@ public class VendingMachineCLI {
 			if (selection.contentEquals("1")) {
 				
 				funds.addAmount(new BigDecimal(1));
+				inventory.auditFeed(new BigDecimal(1), funds.getBalance());
 			} 
 			else if (selection.contentEquals("2")) {
 				
 				funds.addAmount(new BigDecimal(2));
+				inventory.auditFeed(new BigDecimal(2), funds.getBalance());
 			}
 			else if (selection.contentEquals("5")) {
 			
 				funds.addAmount(new BigDecimal(5));
+				inventory.auditFeed(new BigDecimal(5), funds.getBalance());
 			}
 			else if (selection.contentEquals("10")) {
 		
 				funds.addAmount(new BigDecimal(10));
+				inventory.auditFeed(new BigDecimal(10), funds.getBalance());
 			}
 			
 			System.out.println("You have $"+ funds.getBalance());
 		}
+	}
+	
+	public void selectProduct() {
+		boolean valid = false;
+		while (!valid) {
+			System.out.println();
+			System.out.print("Please enter a code for an item (or enter 'Back' to return to purchase menu) >>>");
+		
+			Scanner scanner = new Scanner(System.in);
+			String userCode = scanner.nextLine();
+			
+			if (/*item does not exist*/) {
+				
+			}
+			else if (/*item is sold out*/) {
+				
+			}
+			else if (userCode.toLowerCase().equals("back")) {
+				valid = true;
+			}
+			else {
+				// Print 'Dispensing <item name: item price>"
+				// Print "Money Remaining: x"
+				// Print item type message
+				// Update audit
+				valid = true;
+			}
+		}
+		
 	}
 
 	public static void main(String[] args) {
