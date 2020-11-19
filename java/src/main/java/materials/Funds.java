5f95a77f7449fab29ab555d90787a1387cb5bcd4
Alex Kennedy <rakennedyj18@gmail.com>
2020-11-19 12:20:53 -0500

    Pushing Funds for testing

--- java/src/main/java/materials/Funds.java
+++ java/src/main/java/materials/Funds.java
@@ -10,8 +10,48 @@ public class Funds {
 		return balance;
 	}
 
-	public void setBalance(BigDecimal amountAdded) {
+	public void addAmount(BigDecimal amountAdded) {
 		this.balance = balance.add(amountAdded);
+	}
+	
+	public void subtractAmount(BigDecimal amountSubtracted) {
+		this.balance = balance.subtract(amountSubtracted);
+	}
+	
+	public String getChange (BigDecimal remainingBalance) {
+		
+		BigDecimal quarterValue = new BigDecimal(0.25);
+		BigDecimal dimeValue = new BigDecimal(0.10);
+		BigDecimal nickelValue = new BigDecimal(0.05);
+		BigDecimal changeDue = new BigDecimal (0.0);
+		
+		
+		int quarters = 0;
+		int dimes = 0;
+		int nickels = 0;
+		while (remainingBalance.compareTo(quarterValue) > 0) {
+			
+			remainingBalance.subtract(quarterValue);
+			quarters += 1;
+			changeDue.add(quarterValue);
+			
+		}
+		
+		while (remainingBalance.compareTo(dimeValue) > 0) {
+			
+			remainingBalance.subtract(dimeValue);
+			dimes += 1;
+			changeDue.add(dimeValue);
+		}
+		
+		while (remainingBalance.compareTo(nickelValue) > 0) {
+			
+			remainingBalance.subtract(nickelValue);
+			nickels += 1;
+			changeDue.add(nickelValue);
+			
+		}
+		return "Your change : " + changeDue + "\n" + "Quarters: " + quarters +  "\n" + "Dimes: " + dimes + "\n" + "Nickels: " + nickels;
 	}
 	
 	
--- java/src/main/java/materials/InventoryManagement.java
+++ java/src/main/java/materials/InventoryManagement.java
@@ -1,5 +1,12 @@
 package materials;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryManagement {
+	
+	private List<Sellable> sellables = new ArrayList<Sellable>();
+	
+	
 
 }
