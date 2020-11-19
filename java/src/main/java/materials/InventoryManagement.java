a04884b1dca7c5657eed5fd6813bada7370f68c3
Alex Kennedy <rakennedyj18@gmail.com>
2020-11-19 14:45:07 -0500

    Pushing first part of invetory management for testing

--- java/src/main/java/materials/InventoryManagement.java
+++ java/src/main/java/materials/InventoryManagement.java
@@ -1,12 +1,66 @@
 package materials;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class InventoryManagement {
 	
 	private List<Sellable> sellables = new ArrayList<Sellable>();
 	
+	public int getSize() {
+		return sellables.size();
+	}
 	
+	public Sellable getSellableAt(int index) {
+		return sellables.get(index);
+	}
+	
+	
+	public void restock(String fileName) {
+		File inputFile = new File(fileName);
+		
+		try(Scanner fileScanner = new Scanner(inputFile)){
+		
+		while (fileScanner.hasNextLine()) {
+			String line = fileScanner.nextLine();
+			String [] lineArr = line.split("|");
+			
+			String slotLocation = lineArr[0];
+			String productName = lineArr[1];
+			BigDecimal price = new BigDecimal(lineArr[2]);
+			String category = lineArr[3];
+			if (category.contentEquals("Drink")) {
+				
+				Beverage product = new Beverage(slotLocation, productName, price);
+				sellables.add(product);
+			}
+			
+			if (category.contentEquals("Chip")) {
+				
+				Chips product = new Chips(slotLocation, productName, price);
+				sellables.add(product);
+			}
+			if (category.contentEquals("Gum")) {
+				
+				Gum product = new Gum(slotLocation, productName, price);
+				sellables.add(product);
+			}
+			
+			if (category.contentEquals("Candy")) {
+				
+				Candy product = new Candy(slotLocation, productName, price);
+				sellables.add(product);
+			}
+		}
+	} catch (FileNotFoundException e) {
+		
+		System.out.println("File not found");
+		System.exit(1);
+	}
 
+}
 }
