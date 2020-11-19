c70f5f3696a6d7dc5196e16d68027e988c6f3afc
Alex Kennedy <rakennedyj18@gmail.com>
2020-11-19 11:08:09 -0500

    attempting commit

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -7,6 +7,8 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String[] PURCHASE_MENU = {"Feed Money", "Select Product", "Finish Transaction", "Back"};
+	private static final String[] FEED_MONEY_MENU = {"1", "5", "10", "Back"};
 
 	private Menu menu;
 
@@ -17,12 +19,34 @@ public class VendingMachineCLI {
 	public void run() {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+			System.out.println("You picked: " + choice);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				
+				String selection = "";
+				
+				while(!selection.contentEquals("Back")) {
+				selection = (String) menu.getChoiceFromOptions(PURCHASE_MENU);
+				
+				if (selection.equals("Feed Money")) {
+					processMoney();
+				}
+				
+				System.out.println("You selected from the 2nd level: " + selection);
+				}
 			}
+		}
+	}
+	
+	public void processMoney() {
+		
+		String selection = "";
+		while(!selection.contentEquals("Back")) {
+			
+			selection = (String) menu.getChoiceFromOptions(FEED_MONEY_MENU);
+			
 		}
 	}
 
