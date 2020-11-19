84fc7cfc01ae879f02791155537c62bdf7e54b6c
Alex Kennedy <rakennedyj18@gmail.com>
2020-11-19 15:01:27 -0500

    Pushing inventory management part 2

--- java/src/main/java/materials/InventoryManagement.java
+++ java/src/main/java/materials/InventoryManagement.java
@@ -67,7 +67,7 @@ public class InventoryManagement {
 	public void viewProducts() {
 		for (Sellable product : sellables) {
 			if (product.getQuantity() == 0) {
-				System.out.println(product.getSlotLocation() + " " + product.getName() + " " + product.getPrice() + " SOLD OUT");
+				System.out.println(product.getSlotLocation() + " " + product.getName() + " $" + product.getPrice() + " SOLD OUT");
 			}
 			else {
 				System.out.println(product.getSlotLocation() + " " + product.getName() + " $" + product.getPrice() + product.getQuantity() + " avaialabe");
